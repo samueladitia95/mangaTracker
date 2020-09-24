b0f1f43fb443cb59df841673cab2fac3d3416b53
Samuel Aditia <samueladitia95@gmail.com>
2020-09-24 19:47:37 +0700

    Port Changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const index = require("./routes/index");
 const session = require("express-session");
 
